148f31f30c4c38bffc9a05ce6e84d9f2524f8042
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2022-05-21 00:54:34 -0300

    Update code

---
+++ mfcc/test.py
@@ -0,0 +1,9 @@
+
+
+from typing import List
+
+
+with open("/home/adrianords/Documentos/mfcc_dart_native/mfcc/buffer2", "r") as a:
+    b = a.readline().replace(',', ' ')
+    with open("/home/adrianords/Documentos/mfcc_dart_native/mfcc/buffer2_esp", "w") as c:
+        c.write(b)
