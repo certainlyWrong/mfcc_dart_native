0fcdc96127d4202c5666328a1f4b69154bf8547a
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2022-05-21 00:54:02 -0300

    Update code

--- mfcc/header/mfcc.h
+++ mfcc/header/mfcc.h
@@ -1,76 +1,76 @@
-#ifndef _MFCC_H_
-#define _MFCC_H_
-#include "wav.h"
+/**
+ * @file mfcc.h
+ * @author Adriano R. de Sousa (adrianorodriguesdsousa@gmail.com)
+ * @date 2022-05-20
+ * @version 0.0.1
+ */
 
-#define PI 3.1415926
-#define _DEFAULT_DETECTION_CONTINUOUS_FRAME_NUM_ 5
-#define _DEFAULT_DETECION_THRESHOLD_START_ 20
-#define _DEFAULT_DETECION_THRESHOLD_END_ 10
-#define _FLEXIBLE_RANGED_ 5
-typedef struct
-{
-	double *data;
-	int dataSize;
-} AUDIODATA;
-typedef struct
-{
-	int frameSize;
-	int frameOverlap;
-	int frameNum;
-	int *framePos;
-} FrameInfo;
-typedef struct
-{
-	double *frameData;
-	double *spectrum;
-	int id;
-} Frame;
-typedef struct
-{
-	int sampleRate;
-	int low;
-	int high;
-	int ndft;
-	int filterSize;
-	int *pos_vector;
-} MelBankInfo;
-typedef struct
-{
-	double *filter;
-	int filterSize;
-	int dimension;
-	double *mfcc_vector;
-} MFCCInfo;
-typedef struct
-{
-	int dimension;
-	int frameNum;
-	double **coeff;
-} MFCC_Matrix;
+// The MIT License (MIT)
 
-void audioDataInit(AUDIODATA *au);
-void setFrameInfo(FrameInfo *frameInfo, int frameSize, int frameOverlap, int frameNum);
-double getBaseNoisePower(double *in, int size, int openingFrameCnt, FrameInfo frameInfo);
-void voiceActiveDetection(int *vadStartPos, int *vadEndPos, double *in, int size, int openingFrameCnt, FrameInfo frameInfo);
-void preEmphasize(AUDIODATA *source, double *in, int voiceStartPos, int voiceEndPos, double a);
-void frameInit(Frame *frame);
-void updateFrameData(Frame *frame, FrameInfo frameInfo, double *in);
-void hammingWindow(double *frame, int frameSize, double a);
-void enFrame(FrameInfo *frameInfo, int size);
-void dft_execute(Frame *frame, FrameInfo frameInfo);
-double frequencyToMelScale(double f);
-double melScaleToFrequency(double m);
-int frequencyToPos(double f, int fs, int ndft);
-void triangularWindow(int left, int middle, int right, double *out, double *in);
-void setMelBankInfo(MelBankInfo *melBank, int fs, int low, int high, int ndft, int num);
-void melFilter(MelBankInfo melBank, MFCCInfo *mfcc_std, Frame frame);
-void setMfccDimension(MFCCInfo *mfcc_std, int dim);
-void dct_execute(MFCCInfo *mfcc_std);
-void melBankDestroy(MelBankInfo *melBank);
-void mallocMFCCMatrix(MFCC_Matrix *mfcc_res, int dim, int frameNum);
-void freeMatrix(double **in);
-void updateFrameMfccInfo(MFCC_Matrix *mfcc_std, MFCCInfo frame, int frameIndex);
-void mfcc_execute(MFCC_Matrix *mfcc_std, double *originalData, int size, int sampleRate, int dim, int frameSize, int frame_overlap, int melFilterNum);
-void mfcc_diff_execute(MFCC_Matrix *mfcc_std, MFCC_Matrix *mfcc_diff_1);
-void dump(Frame *frame, MFCCInfo *mfcc);
-#endif
+// Copyright (c) 2022 Adriano R. de Sousa
+
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+
+#ifndef _MFCC_
+
+#ifdef _MFCC_private
+#include <math.h>
+#include <stdlib.h>
+#include <complex.h>
+
+double *MFCC_cp_buffer(double *Buffer, int sizeBuffer);
+void MFCC_normilize(MFCC_input *signal);
+
+typedef struct mfcc
+{
+};
+
+#endif // _MFCC_private
+#include <stdint.h>
+
+typedef enum
+{
+	FALSE,
+	TRUE
+} Bool;
+
+typedef struct
+{
+	double *buffer;
+	uint16_t size;
+} MFCC_coef;
+
+typedef struct
+{
+	double *buffer;
+	size_t sizeBuffer;
+	uint16_t hopSize,
+			fftSize,
+			dctFilterNum,
+			melFilterNum;
+	Bool normilizeActivate;
+} MFCC_input;
+
+MFCC_input *MFCC_initDefaultValues(double *buffer, size_t sizeBuffer);
+
+MFCC_coef MFCC_execute(MFCC_input *signal);
+
+void MFCC_input_free(MFCC_input *signal);
+
+#endif // _MFCC_
--- mfcc/src/main.c
+++ mfcc/src/main.c
@@ -1,7 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../header/mfcc.h"
 
+void loadBuffer(double *, size_t, char *);
+// 110250
 
-int main()
+int main(int argc, char const *argv[])
 {
+	double *buffer;
+	size_t bufferSize = 110250;
+	char *path = "/home/adrianords/Documentos/mfcc_dart_native/mfcc/buffer1_esp";
+	loadBuffer(buffer, bufferSize, path);
 
-    return 0;
+		return 0;
+}
+
+void loadBuffer(double *buffer, size_t sizeBuffer, char *path)
+{
+	FILE *aux = fopen(path, "r");
+	buffer = (double *)malloc(sizeof(double) * sizeBuffer);
+
+	for (size_t i = 0; i < sizeBuffer; i++)
+	{
+		fscanf(aux, "%lf", &buffer[i]);
+		printf("hello\n");
+	}
+
+	fclose(aux);
 }
